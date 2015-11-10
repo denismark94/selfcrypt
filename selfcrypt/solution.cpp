df34f77f8ce9a2dd416763c28ac46a9c69d3b1a0
unknown <admin@w12-416-23.main.sgu.ru>
2015-11-10 15:26:41 +0300

    First Commit

---
+++ selfcrypt/solution.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+using namespace std;
+void execute() 
+{
+	printf("Расшифровка успешна");
+}
+
+int main (int argc, char * argv) 
+{
+	execute();
+	System("PAUSE");
+	return 0;
+}
+
