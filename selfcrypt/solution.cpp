ce35fa169970ae77aff7e150b80bca97134395a1
Denis Markin <lexx129>
2015-11-12 10:03:16 +0300

    WriteProcessMemory is probably the solution Trying to get address of function in exe file

--- selfcrypt/solution.cpp
+++ selfcrypt/solution.cpp
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
 
 using namespace std;
 void execute() 
 {
-	printf("Расшифровка успешна");
+	printf("Secret function is active!\n");
 }
 
-int main (int argc, char * argv) 
-{
-	execute();
-	System("PAUSE");
+void stub(){}
+
+int main (int argc, char** argv) 
+{	
+	/*HANDLE h = OpenProcess(PROCESS_VM_OPERATION
+		|PROCESS_VM_WRITE,
+		true,GetCurrentProcessId());*/
+	printf("%p\n",execute);
+	std::ifstream ifs(argv[0],ifstream::in 
+		|ifstream::binary);
+	//ifs.seekg
+	//WriteProcessMemory(h,);
+	//execute();
+	system("PAUSE");
 	return 0;
 }
 
